2c0125c61e81ad44996a395b256b4df47a70c1ad
Jims <73562149+jimmycamangon@users.noreply.github.com>
2023-08-07 01:10:57 +0800

    Update projects.tsx

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -5,7 +5,7 @@ const projects: Project[] = [
         name: "DACCS (Digital Application of Certification and Clearance System)",
         description:
             "Simplify and digitize the process of requesting and obtaining essential certifications and clearances for residents of Barangay Caingin.",
-        image: "/../public/DACCS.JPG",
+        image: "DACCS.JPG",
         screenshots: [
             "DACCS_Admin Dashboard.JPG",
             "DACCS_Claiming List.JPG",
@@ -16,7 +16,7 @@ const projects: Project[] = [
     {
         name: "QRS (Queue Reservation System)",
         description: "Streamline and optimize the queuing experience for students at St. Vincent School's registration and cashier offices through an online reservation system.",
-        image: "/../public/QRS.JPG",
+        image: "QRS.JPG",
         screenshots: [
             "QRS_In Queue Page.JPG",
             "QRS_HomePage.JPG",
@@ -27,7 +27,7 @@ const projects: Project[] = [
     {
         name: "AMS (Attendance Monitoring System)",
         description: "Enhance attendance tracking and management for grades 2 and 3 students North Marinig Elementary School.",
-        image: "/../public/AMS.JPG",
+        image: "AMS.JPG",
         screenshots: [
             "AMS_Admin Page.JPG",
             "AMS_History.JPG",
@@ -38,7 +38,7 @@ const projects: Project[] = [
     {
         name: "Voting System",
         description: "Revolutionize the election process at AICS by enabling students to cast their votes online for various positions and initiatives, ensuring transparency and efficiency.",
-        image: "/../public/Voting_System.JPG",
+        image: "Voting_System.JPG",
         screenshots: [
             "VS_Admin Dashboard.JPG",
             "VS_Admin Profile.JPG",
