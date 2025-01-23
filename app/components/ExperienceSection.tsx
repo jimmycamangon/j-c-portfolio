4c8e390fe7bd7fb86c9ada774134e087367b3ef5
zhask09 <73562149+jimmycamangon@users.noreply.github.com>
2025-01-23 09:58:36 +0800

    Update as of Jan. 23, 2025
    Update Experience Section

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -21,12 +21,12 @@ const ExperienceSection = () => {
                     <div className="text-primaryColor font-bold text-2xl py-2 dark:text-secondaryColor">Programmer</div>
                     <span className="font-bold">Amalgamated Specialties Corporation (AMSPEC) • Muntinlupa, Metro Manila</span><br />
                     <span className="font-normal italic">September 2023 to Present</span><br />
-                    <li className="text-left">Contributed as a part of the MIS team, devising strategies to uphold compliance with the technology industry.</li>
-                    <li className="text-left">Effectively managed daily bug fixes and incorporated new features into WinForms applications using C# Language and MVC methodology.</li>
-                    <li className="text-left">Implemented Crystal Reports (SAP) for streamlined report generation, guaranteeing accuracy and polished presentation of data.</li>
-                    <li className="text-left">Successfully transitioned from PHP to C# in a short period, demonstrating adaptability and a strong learning curve.</li>
-                    <li className="text-left">Proficient in using Stored Procedures to optimize database operations.</li>
-                    <li className="text-left">Executed Object-Oriented Programming (OOP) procedures to improve code structure and maintainability.</li>
+                    <li className="text-left">Independently developed and maintained an <b>Enterprise System</b>, with a primary focus on <b>WinForms applications</b>, showcasing adaptability to the environment and rapid learning in a short period.</li>
+                    <li className="text-left">Completed the previously unfinished <b>Accounts Receivable Module</b>, ensuring its functionality and alignment with business requirements.</li>
+                    <li className="text-left">Nearing completion of the <b>Accounts Payable Module</b>, showcasing consistency in delivering critical financial system components.</li>
+                    <li className="text-left">Proficient in creating and optimizing <b>Stored procedures</b> and handling <b>SQL Server</b> operations, addressing complex database requirements efficiently.</li>
+                    <li className="text-left">Implemented effective <b>preventive measures and solutions</b> to address recurring company challenges, such as <b>data fetching optimizations</b> and system improvements.</li>
+                    <li className="text-left">Added a feature for company website, such as a <b>dynamic price change functionality</b>, which was previously unavailable, contributing to improved operational efficiency.</li>
                 </div>
             </div>
         </div>
@@ -34,7 +34,7 @@ const ExperienceSection = () => {
 
         {/* End */}
         <h1 className="pt-10 font-bold text-1xl text-primaryColor dark:text-secondaryColor md:p-6 p-8">
-            Core Technologies: <span className="text-grayColor font-semibold dark:text-whiteColor">C# | SQL Server | SAP Report | MVC</span>
+            Core Technologies: <span className="text-grayColor font-semibold dark:text-whiteColor">C# | PHP | SQL Server | Crystal Report</span>
         </h1>
     </div>
 </section>
